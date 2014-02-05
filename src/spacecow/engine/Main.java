bb9c873f4b88a9b4f958c527a6418d03f5779cf2
Gurey <gustavholfve@Gurey-Retina.local>
2014-02-05 15:54:17 +0100

    Load natives

--- src/spacecow/engine/Main.java
+++ src/spacecow/engine/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	static Game g;	
 	public static void main(String[] args) {
 		
-		//Load natives to be able to run the rest of the program.
-//		LoadNative nativeLoader = new LoadNative();
-//		nativeLoader.loadNatives();
+//		Load natives to be able to run the rest of the program.
+		LoadNative nativeLoader = new LoadNative();
+		nativeLoader.loadNatives();
 		new Game().start();
 	}
 }
